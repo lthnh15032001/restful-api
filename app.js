e7277c8befe13923d30625ac9e39b4a100bcc6ce
lethanhdatdev <lethanhdatdev1503@gmail.com>
2020-10-13 14:06:26 +0700

    api start up

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const app = express();
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv/config')
-const postRoute = require('./routes/posts');
-const taskRoute = require('./routes/taskRoute')
-const userRoute = require('./routes/users');
-const courseRoute = require('./routes/course')
-const tagRoute = require('./routes/tag')
-const todoRoute = require('./routes/todoRoute')
-const noteRoute = require('./routes/noteRoute')
-const starFindRoute = require('./routes/starFindRoute')
+// const postRoute = require('./routes/posts');
+// const taskRoute = require('./routes/taskRoute')
+// const userRoute = require('./routes/users');
+// const courseRoute = require('./routes/course')
+// const tagRoute = require('./routes/tag')
+// const todoRoute = require('./routes/todoRoute')
+// const noteRoute = require('./routes/noteRoute')
+// const starFindRoute = require('./routes/starFindRoute')
+const users = require('./devC/routes/users')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json');
 const cors = require('cors')
@@ -20,14 +21,14 @@ app.use(bodyParser.json())
 console.log(port)
 
 
-app.use('/posts', postRoute)
-app.use('/users', userRoute)
-app.use('/courses', courseRoute)
-app.use('/tasks', taskRoute)
-app.use('/note', noteRoute)
-app.use('/tag', tagRoute)
-app.use('/todoList', todoRoute)
-app.use('/findTag', starFindRoute)
+// app.use('/posts', postRoute)
+app.use('/users', users)
+// app.use('/courses', courseRoute)
+// app.use('/tasks', taskRoute)
+// app.use('/note', noteRoute)
+// app.use('/tag', tagRoute)
+// app.use('/todoList', todoRoute)
+// app.use('/findTag', starFindRoute)
 app.use("/data", express.static(__dirname + '/data'));
 app.get('/', swaggerUi.serve, (req, res) => {
     res.send("Welcome to DevC Raw Team's Restful API ")
---
+++ devC/routes/users.js
@@ -0,0 +1,87 @@
+const express = require('express')
+
+const router = express.Router();
+
+const User = require('../models/Users')
+const { registerValidation, LoginValidation } = require('../validation')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const tokenList = {}
+router.post('/register', async (req, res) => {
+    // validate data before add usersssss
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+    // check if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) {
+        return res.status(400).send("Email đã tồn tại ")
+    }
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+    // create new users
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword
+    });
+    try {
+        const savedUser = await user.save();
+        res.send({ user: user._id });
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+//Login
+
+router.post('/login', async (req, res) => {
+    // validate data before add usersssss
+    const { error } = LoginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+    // check if the email exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).send("Email khong ton tai")
+    }
+    // password is correct ??
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send("Sai password")
+
+    //Create and asssign a token
+    const token = jwt.sign(
+        {
+            _id: user.id
+        },
+        process.env.TOKEN_SECRET,
+        {
+            expiresIn: process.env.tokenLife
+        }
+    )
+    const refreshToken = jwt.sign(
+        {
+            _id: user.id
+        },
+        process.env.TOKEN_SECRET,
+        {
+            expiresIn: process.env.refreshTokenLife
+        }
+    )
+    const response = {
+        "token": token,
+        "status": "login",
+        "refreshToken": refreshToken
+    }
+    tokenList[refreshToken] = response
+    // res.header('auth_token', token)
+
+    // res.json(tokenList)
+    res.json({"token":token, "code":200})
+
+})
+module.exports = router
+
+// {
+// 	"email": "lethanhdatdev1222ss03@gmail.com",
+// 	"password": "121212"
+// } 
