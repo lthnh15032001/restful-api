27dc433e9e1cf5e9d9daff5e141e577441bfa970
lethanhdatdev <lethanhdatdev1503@gmail.com>
2020-10-13 15:07:03 +0700

    config path

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+
+"use strict";
+// import { Router } from "express";
+// const router = Router();
+const express = require('express')
+const router = express.Router()
+router.get('/', async (req, res) => {
+    res.send("hello my friend oop user")
+    // try {
+    //     const courseList = await course.find()
+    //     res.json(courseList)
+    // } catch (err) {
+    //     res.json({ message: err })
+    // }
+})
+
+// export default router;
+module.exports = router;
