bd2eca571575177316fc68506a901f4699c5b06c
lethanhdatdev <lethanhdatdev1503@gmail.com>
2020-10-13 20:21:03 +0700

    set up body parser

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,37 @@ router.get('/', async (req, res) => {
     }
 })
 router.post('/', async (req, res) => {
+    const { profile, updated_at, created_at, last_active } = req.body;
+    const {
+        full_name,
+        avatar,
+        birthday,
+        emails,
+        phones,
+        addresses,
+        passport,
+        occupations,
+        id_card
+    } = profile;
+    const UsersModel = new userSchema({
+        profile: {
+            full_name: full_name,
+            avatar: avatar,
+            birthday: birthday,
+            emails: emails,
+            phones: phones,
+            addresses: addresses,
+            occupations: occupations,
+            passport: passport,
+            id_card: id_card
+        },
+        updated_at: updated_at,
+        created_at: created_at,
+        last_active: last_active
+    })
     try {
-        const users = await userSchema.find()
-        res.json({req: req.body})
+        const usersSave = await UsersModel.save()
+        res.json(usersSave)
     } catch (err) {
         res.json({ message: err })
     }
