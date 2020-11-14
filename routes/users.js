927d39d1b5c970caaec1503dff5a416ab56d52bc
lethanhdatdev <lethanhdatdev1503@gmail.com>
2020-11-14 16:34:22 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -14,38 +14,38 @@ router.get('/', async (req, res) => {
 
 
 router.post('/', async (req, res) => {
-        const { profile, updated_at, created_at, last_active, spending, debt, income, id_fb } = req.body;
-        const {
-            full_name,
-            avatar,
-            birthday,
-            emails,
-            phones,
-            addresses,
-            passport,
-            occupations,
-            id_card
-        } = profile;
-        const UsersModel = new Users({
-            id_fb: id_fb,
-            profile: {
-                full_name: full_name,
-                avatar: avatar,
-                birthday: birthday,
-                emails: emails,
-                phones: phones,
-                addresses: addresses,
-                occupations: occupations,
-                passport: passport,
-                id_card: id_card
-            },
-            spending: spending,
-            debt: debt,
-            income: income,
-            updated_at: updated_at,
-            created_at: created_at,
-            last_active: last_active
-        })
+    const { profile, updated_at, created_at, last_active, spending, debt, income, id_fb } = req.body;
+    const {
+        full_name,
+        avatar,
+        birthday,
+        emails,
+        phones,
+        addresses,
+        passport,
+        occupations,
+        id_card
+    } = profile;
+    const UsersModel = new Users({
+        id_fb: id_fb,
+        profile: {
+            full_name: full_name,
+            avatar: avatar,
+            birthday: birthday,
+            emails: emails,
+            phones: phones,
+            addresses: addresses,
+            occupations: occupations,
+            passport: passport,
+            id_card: id_card
+        },
+        spending: spending,
+        debt: debt,
+        income: income,
+        updated_at: updated_at,
+        created_at: created_at,
+        last_active: last_active
+    })
     try {
         const idFbExist = await Users.findOne({ id_fb: id_fb })
         if (idFbExist) {
@@ -63,40 +63,50 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.post('/:userId', async (req, res) => {
-    const { profile, updated_at, created_at, last_active, spending, debt, income } = req.body;
-    const {
-        full_name,
-        avatar,
-        birthday,
-        emails,
-        phones,
-        addresses,
-        passport,
-        occupations,
-        id_card
-    } = profile;
+router.post('/updateDebt/:userId', async (req, res) => {
+    const { debt } = req.body
+    if (!debt) {
+        res.json({ error: "Nhập đầy đủ", })
+    }
     try {
         const _id = req.params.userId;
         const updateUser = await Users.updateOne({ _id: _id }, {
-            $set: {
-                profile: {
-                    full_name: full_name,
-                    avatar: avatar,
-                    birthday: birthday,
-                    emails: emails,
-                    phones: phones,
-                    addresses: addresses,
-                    occupations: occupations,
-                    passport: passport,
-                    id_card: id_card
-                },
-                spending: spending,
-                debt: debt,
-                income: income,
-                updated_at: updated_at,
-                created_at: created_at,
-                last_active: last_active
+            $push: {
+                debt: debt
+            }
+        })
+        res.json(updateUser)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+router.post('/updateIncome/:userId', async (req, res) => {
+    const { income } = req.body
+    if (!income) {
+        res.json({ error: "Nhập đầy đủ", })
+    }
+    try {
+        const _id = req.params.userId;
+        const updateUser = await Users.updateOne({ _id: _id }, {
+            $push: {
+                income: income
+            }
+        })
+        res.json(updateUser)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+router.post('/updateSpending/:userId', async (req, res) => {
+    const { spending } = req.body
+    if (!spending) {
+        res.json({ error: "Nhập đầy đủ", })
+    }
+    try {
+        const _id = req.params.userId;
+        const updateUser = await Users.updateOne({ _id: _id }, {
+            $push: {
+                spending: spending
             }
         })
         res.json(updateUser)
